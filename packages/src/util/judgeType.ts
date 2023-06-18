01d5a1e9c46d1e0805204d446f08eebc36eb6ae9
OceanH <1404939185@qq.com>
2023-06-18 21:11:15 +0800

    feat(util): ✨ 添加数据类型判断

---
+++ packages/src/util/judgeType.ts
@@ -0,0 +1,41 @@
+/**
+ * 数据类型判断
+ */
+
+export function isNumber(value: any) {
+  return !isNaN(Number(value))
+}
+
+export function isObject(value: any) {
+  return Object.prototype.toString.call(value) === '[object Object]'
+}
+
+export function isFunction(value: any) {
+  return typeof value === 'function'
+}
+
+export function isArray(value: any) {
+  if (typeof Array.isArray === 'function') {
+    return Array.isArray(value)
+  } else {
+    return Object.prototype.toString.call(value) === '[object Array]'
+  }
+}
+
+export function isString(value: any) {
+  return typeof value === 'string'
+}
+
+export function isBoolean(value: any) {
+  return typeof value === 'boolean'
+}
+
+export function isEmpty(value: any) {
+  if (isArray(value)) {
+    return value.length === 0
+  }
+  if (isObject(value)) {
+    return Object.keys(value).length === 0
+  }
+  return value === '' || value === undefined || value === null
+}
